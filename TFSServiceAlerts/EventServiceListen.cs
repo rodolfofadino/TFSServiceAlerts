03a8b6255673fde2ba858c9b911c4da43ee17aec
rodolfofadino <rodolfo.fadino@gmail.com>
2013-03-30 12:24:59 -0300

    servico configurado

--- TFSServiceAlerts/EventServiceListen.cs
+++ TFSServiceAlerts/EventServiceListen.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace TFSServiceAlerts
 {
@@ -6,7 +9,39 @@ namespace TFSServiceAlerts
     {
         public void Notify(string eventXml, string tfsIdentityXml)
         {
-            throw new NotImplementedException();
+            var xml = XElement.Parse(eventXml);
+
+            switch (xml.Name.LocalName)
+            {
+                case "CheckinEvent":
+                    var checkinEvent = DeserializeXmlToType<CheckinEvent>(eventXml);
+                    Console.WriteLine("Checkin");
+                    Console.WriteLine("Autor:{0}",checkinEvent.Committer);
+                    Console.WriteLine("Comments:{0}",checkinEvent.Comment);
+                    Console.WriteLine("");
+                    //TODO: persistir evento
+                    break;
+                case "BuildCompletionEvent":
+                    var buildEvent = DeserializeXmlToType<BuildCompletionEvent>(eventXml);
+                    Console.WriteLine("Build");
+                    Console.WriteLine("Title:{0}",buildEvent.Title);
+                    Console.WriteLine("Status:{0}",buildEvent.CompletionStatus);
+                    Console.WriteLine("");
+                    //TODO: persistir evento
+                    break;
+                default:
+                    throw new NotSupportedException("Evento não suportado");
+            }
+        }
+
+        private static T DeserializeXmlToType<T>(string eventXml) where T : class
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var reader = new StringReader(eventXml))
+            {
+                return serializer.Deserialize(reader) as T;
+            }
         }
     }
 }
