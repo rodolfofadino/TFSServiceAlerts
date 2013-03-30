edec78c09c871fa0e983eccd76274d6b489943cd
rodolfofadino <rodolfo.fadino@gmail.com>
2013-03-30 12:10:37 -0300

    criado estrutura de deserializacao do xml do eventXml

---
+++ TFSServiceAlerts/BuildCompletionEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace TFSServiceAlerts
+{
+    [XmlRoot]
+    public class BuildCompletionEvent
+    {
+        public string TeamFoundationServerUrl { get; set; }
+        public string TeamProject { get; set; }
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string CompletionStatus { get; set; }
+        public string Subscriber { get; set; }
+        public string Configuration { get; set; }
+        public string RequestedBy { get; set; }
+        public string TimeZone { get; set; }
+        public string TimeZoneOffset { get; set; }
+        public string BuildStartTime { get; set; }
+        public string BuildCompleteTime { get; set; }
+        public string BuildMachine { get; set; }
+    }
+}
