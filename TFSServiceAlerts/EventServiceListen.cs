8b707e9d224cbaab94a5415644e720ec00da4ef2
rodolfofadino <rodolfo.fadino@gmail.com>
2013-03-29 22:07:50 -0300

    Criado o servico que receba os alertas

---
+++ TFSServiceAlerts/EventServiceListen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TFSServiceAlerts
+{
+    public class EventServiceListen:IEventServiceListen
+    {
+        public void Notify(string eventXml, string tfsIdentityXml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
