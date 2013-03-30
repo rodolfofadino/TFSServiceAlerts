b319b8ab63a0df9d051d37d28dfc3ec4f663c992
rodolfofadino <rodolfo.fadino@gmail.com>
2013-03-29 21:49:42 -0300

    Interface do servico criada

---
+++ TFSServiceAlerts/IEventServiceListen.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace TFSServiceAlerts
+{
+    [ServiceContract(Namespace = "http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03")]
+    interface IEventServiceListen
+    {
+        [OperationContract(Action = "http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03/Notify")]
+        [XmlSerializerFormat(Style = OperationFormatStyle.Document)]
+        void Notify(string eventXml, string tfsIdentityXml);
+    }
+}
