df18b319ae2cbbaff243766bc1f9f867a3a62efc
rodolfofadino <rodolfo.fadino@gmail.com>
2013-03-29 22:19:44 -0300

    configuracoes e servico hospedado

--- TFSServiceAlerts/Program.cs
+++ TFSServiceAlerts/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 
 namespace TFSServiceAlerts
@@ -9,6 +10,12 @@ namespace TFSServiceAlerts
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(EventServiceListen)))
+            {
+                host.Open();
+                Console.WriteLine("Serviço iniciado");
+                Console.ReadLine();
+            }
         }
     }
 }
